1e9fff73025c62a7b57d9b18904437f97e944139
SilverBulletCY <545571461@qq.com>
2021-09-10 23:13:18 +0800

    commit

--- Graph_AdjMatrix.cpp
+++ Graph_AdjMatrix.cpp
@@ -1,18 +1,34 @@
 // Operations on Graph stored as AdjMatrix
 #define MAXN 20
+#define INF 0x3fffffff
 
 struct graph {
 	int G[MAXN][MAXN];
 	int vnum, arcnum;
 };
-bool visited[MAXN] = { false };
 
+bool visited[MAXN];
+int dist[MAXN];
+int path[MAXN];
+
+void initArray(graph* g);
 graph* createGraph(int vnum, int arcnum);
 void DFS_Traverse(graph* g, bool visited[]);
 void DFS(graph* g, int i);
 void BFS_Traverse(graph* g, bool visited[]);
 void resetVisited(graph* g, bool visited[]);
+int findMinDist(graph* g, bool visited[], int dist[], int i);
+void Dijkstra(graph* g, bool visited[], int dist[], int path[]);
+void print(graph* g, int data[]);
 
+
+void initArray(graph* g) {
+	for (int i = 0; i < g->vnum; i++) {
+		visited[i] = false;
+		dist[i] = INF;
+		path[i] = -1;
+	}
+}
 
 graph* createGraph(int vnum, int arcnum) {
 	graph* g = new graph;
@@ -43,7 +59,7 @@ void DFS(graph* g, int i) {
 	visited[i] = true;
 	cout << i << " ";
 	for (int j = 0; j < g->vnum; j++) {
-		if (g->G[i][j]  && (!visited[j])) {
+		if (g->G[i][j] && (!visited[j])) {
 			DFS(g, j);
 		}
 	}
@@ -56,7 +72,7 @@ void BFS_Traverse(graph* g, bool visited[]) {
 			visited[i] = true;
 			cout << i << " ";
 			q.push(i);
-			while (!q.empty()){
+			while (!q.empty()) {
 				q.pop();
 				for (int j = 0; j < g->vnum; j++) {
 					if (g->G[i][j] && (!visited[j])) {
@@ -74,4 +90,38 @@ void resetVisited(graph* g, bool visited[]) {
 	for (int i = 0; i < g->vnum; i++) {
 		visited[i] = false;
 	}
+}
+
+int findMinDist(graph* g, bool visited[], int dist[], int i) {
+	int minIndex = i;
+	for (int j = i; j < g->vnum; j++) {
+		if ((!visited[j]) && dist[j] < dist[minIndex]) {
+			minIndex = j;
+		}
+	}
+	return minIndex;
+}
+
+void Dijkstra(graph* g, bool visited[], int dist[], int path[]) {
+	dist[0] = 0;
+	for (int i = 0; i < g->vnum; i++) {
+		int v = findMinDist(g, visited, dist, i);
+		if (v == -1) break;
+		visited[v] = true;
+		for (int w = 0; w < g->vnum; w++) {
+			if (g->G[v][w] && (!visited[w])) {
+				if (dist[v] + g->G[v][w] < dist[w]) {
+					dist[w] = dist[v] + g->G[v][w];
+					path[w] = v;
+				}
+			}
+		}
+	}
+}
+
+void print(graph* g, int data[]) {
+	for (int i = 0; i < g->vnum; i++) {
+		cout << data[i] << " ";
+	}
+	cout << endl;
 }
